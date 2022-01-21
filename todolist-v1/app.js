9d3aebc9d6f04d9df8ce1b3f0a4e93543a8058e5
ujangaja <ujangaja@gmail.com>
2022-01-21 16:38:23 +0700

    268. Passing Data from Your Webpage to Your Server

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -3,50 +3,30 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+var items = ['Buy Food', 'Cook Food', 'Eat Food'];
+
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = '';
 
-  /* if (currentDay === 6 || currentDay === 0) {
-    day = 'Weekend';
-    // res.sendFile(__dirname + '/weekend.html');
-  } else {
-    day = 'Weekday';
-    // res.sendFile(__dirname + '/weekday.html');
-  } */
+  var options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+  };
 
-  switch (currentDay) {
-    case 0:
-      day = 'Sunday';
-      break;
-    case 1:
-      day = 'Monday';
-      break;
-    case 2:
-      day = 'Tuesday';
-      break;
-    case 3:
-      day = 'Wednesday';
-      break;
-    case 4:
-      day = 'Thursday';
-      break;
-    case 5:
-      day = 'Friday';
-      break;
-    case 6:
-      day = 'Saturday';
-      break;
+  var day = today.toLocaleDateString('en-US', options);
 
-    default:
-      console.log('Error: current day is equal to:' + currentDay);
-      break;
-  }
+  res.render('list', { kindOfDay: day, newListItems: items });
+});
 
-  res.render('list', { kindOfDay: day });
+app.post('/', function (req, res) {
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect('/');
 });
 
 app.listen(3000, () => {
